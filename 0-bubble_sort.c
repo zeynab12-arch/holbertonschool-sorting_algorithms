a88cf8e1652465969baf8ee5806ee20ef40e1d8a
zeynab12-arch <zeynabshikhaliyeva12@gmail.com>
2026-02-14 16:06:56 +0400

    Add bubble_sort function for sorting integers
    Implement bubble sort algorithm to sort an array of integers in ascending order.

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,1 +1,38 @@
+#include "sort.h"
 
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * using the Bubble sort algorithm
+ * @array: array to sort
+ * @size: size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int tmp;
+	int swapped;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0;
+
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+
+				print_array(array, size);
+				swapped = 1;
+			}
+		}
+
+		if (swapped == 0)
+			break;
+	}
+}
